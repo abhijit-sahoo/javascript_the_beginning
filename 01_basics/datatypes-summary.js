d91cb49857f7299caac756baf2ff72c528832e6d
Abhijit Sahoo <92908633+abhijit-sahoo@users.noreply.github.com>
2024-07-27 09:41:24 +0000

    summary of datatype

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,26 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+// Javascript uses dynamic typing. no required to define the variable to store respective value
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId); //false
+
+
+const bigNumber = 4364365375335555555555555n // bigInt
+
+
+// Reference (Non primitive)
+// Array, Object, Functions
+
+const heros = ["saktiman","naagraj","doga"];
+let myoObj={
+    name: "Abhijit",
+    age: 24
+};
+
+
