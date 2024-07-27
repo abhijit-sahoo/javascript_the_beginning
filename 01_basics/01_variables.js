138203e3e8034b94c47b7054d61bb4bff4a4b24d
Abhijit Sahoo <92908633+abhijit-sahoo@users.noreply.github.com>
2024-07-27 07:04:38 +0000

    Learnt variable in JS

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 144553
+let accountEmail= "abhijit@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur" // prefer not to declare like this
+
+let accoumtState; // undefined 
+
+console.table([accountId,accountEmail, accountPassword, accountCity])
+
+// accountId = 2 // not allowed // TypeError: Assignment to constant variable.
+
+accountEmail = "hc@hc.com"
+accountPassword = "12121212"
+accountCity = "Bengaluru"
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+
+*/
+
+console.log(accountId)
+console.table([accountId,accountEmail, accountPassword, accountCity, accoumtState])
