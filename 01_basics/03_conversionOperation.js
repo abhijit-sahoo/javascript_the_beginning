c3f3546f14532a7866848091555dacde92f98700
Abhijit Sahoo <92908633+abhijit-sahoo@users.noreply.github.com>
2024-07-27 07:53:10 +0000

    learnt undefined with type conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -2,6 +2,7 @@ let score1 = 33
 let score2 = "33"
 let score3 = "33abc" 
 let score4 = null
+let score5;
 
 let valueInNumber2 = Number(score2)  //type conversion
 console.log(typeof (score2))         //string---Not Confusing
@@ -16,3 +17,5 @@ console.log(valueInNumber3)          //NaN (Not a Number)why?
 let valueInNumber4 = Number(score4)
 console.table([score4,typeof (score4),valueInNumber4] ) //null, object, 0
 
+let valueInNumber5 = Number(score5)
+console.table([score5,typeof (score5),valueInNumber5] ) // undefined, undefined, NaN
