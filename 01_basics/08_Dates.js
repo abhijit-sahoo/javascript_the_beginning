e1cfc8938b768dcbffc0c3a37183bbf11bf7d78d
Abhijit Sahoo <92908633+abhijit-sahoo@users.noreply.github.com>
2024-07-29 09:57:23 +0000

    learnt Date

---
+++ 01_basics/08_Dates.js
@@ -0,0 +1,23 @@
+let myDate = new Date();
+
+console.log(myDate);// 2024-07-29T09:44:25.051Z
+console.log(myDate.toString());// Mon Jul 29 2024 09:44:25 GMT+0000 (Coordinated Universal Time)
+console.log(myDate.toDateString()); // Mon Jul 29 2024
+console.log(myDate.toISOString()); // 2024-07-29T09:44:25.051Z
+console.log(myDate.toJSON()); // 2024-07-29T09:44:25.051Z
+console.log(myDate.toLocaleDateString()); // 7/29/2024
+console.log(myDate.toLocaleString()); // 7/29/2024, 9:44:25 AM
+console.log(myDate.toLocaleTimeString());// 9:44:25 AM
+console.log(myDate.toTimeString());// 09:44:25 GMT+0000 (Coordinated Universal Time)
+
+console.log(typeof(myDate)); //object
+
+//+++++++++ Declaration of Date ++++++++// date 
+
+let myCreateDate = new Date(2024, 2, 23);// months atarts from 0 to 11// if it declared as 12 or 13 output will be// 2025-01-23T00:00:00.000Z // Thu Jan 23 2025
+console.log(myCreateDate);// 2024-03-23T00:00:00.000Z
+console.log(myCreateDate.toDateString());// Sat Mar 23 2024
+
+let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+console.log(myCreatedDate.toLocaleString()); // 1/23/2023, 5:03:00 AM
+
