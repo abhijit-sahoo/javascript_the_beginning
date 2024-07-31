6421fec76d0d0863062204239d16cd71e1d6216d
Abhijit Sahoo <92908633+abhijit-sahoo@users.noreply.github.com>
2024-07-31 05:41:16 +0000

    object part_2

---
+++ 02_basics/04_objects_Part_2.js
@@ -0,0 +1,20 @@
+
+// Object In depth
+/************************************** */
+
+//const tinderUserTwo = {} // Non Singleton Object
+//console.log(tinderUserTwo) // output: {} //
+
+const tinderUser = new Object() // Singleton Object
+//console.log(tinderUser) // output: {} // 
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser)// output: { id: '123abc', name: 'Sammy', isLoggedIn: false }
+
+
+
+ 
+
