4c3b58a17ae4985d9fb0acf5fd05b90c80d3be1a
Abhijit Sahoo <92908633+abhijit-sahoo@users.noreply.github.com>
2024-08-05 06:28:13 +0000

    large commits

---
+++ 05_iterations/01_for_loop.js
@@ -0,0 +1,17 @@
+// for loop
+
+
+// for (let index = 0; index < Array.length; index++){
+//     const element = array[index]
+//     console.log(element)
+
+// }
+
+for (let i=0; i<=10; i++){
+    const element = i;
+    if(element == 5){
+        console.log("5 is best number")
+
+    }
+    console.log(element)
+}
