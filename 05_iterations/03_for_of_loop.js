e51f648f20d9a311c179ffcdfb22e1cacad15da9
Abhijit Sahoo <92908633+abhijit-sahoo@users.noreply.github.com>
2024-08-05 10:17:14 +0000

    higher order array

---
+++ 05_iterations/03_for_of_loop.js
@@ -0,0 +1,52 @@
+//for of loop
+
+//["","",""]
+//[{},{},{}]
+
+
+
+//String, Array, TypedArray, Map and Set are all iterables, because their prototype objects have a Symbol.iterator method
+//Syntax: for (const/let variable of iterable)
+//          {statement}
+// for of
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    //console.log(num);
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    //console.log(`Each char is ${greet}`)
+}
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+}
+
+
+
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+
+///////objects are not iterable
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+    
+// }
